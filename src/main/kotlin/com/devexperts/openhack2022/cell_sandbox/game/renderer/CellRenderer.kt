b9bb9349e7a143a9510456ca44a9970ba8367aa1
David <david-d25@yandex.ru>
2022-10-21 03:07:43 +0200

    fixed some graphics issues

--- src/main/kotlin/com/devexperts/openhack2022/cell_sandbox/game/renderer/CellRenderer.kt
+++ src/main/kotlin/com/devexperts/openhack2022/cell_sandbox/game/renderer/CellRenderer.kt
@@ -3,13 +3,21 @@ package com.devexperts.openhack2022.cell_sandbox.game.renderer
 import com.devexperts.openhack2022.cell_sandbox.game.CellState
 import com.devexperts.openhack2022.cell_sandbox.game.World
 import com.devexperts.openhack2022.cell_sandbox.geom.Vector2
+import com.devexperts.openhack2022.cell_sandbox.geom.Vector2.RotationDirection.CLOCKWISE
+import com.devexperts.openhack2022.cell_sandbox.geom.Vector2.RotationDirection.COUNTERCLOCKWISE
 import com.devexperts.openhack2022.cell_sandbox.geom.testCirclesIntersection
 import com.devexperts.openhack2022.cell_sandbox.geom.testLineAndCircleIntersection
 import com.devexperts.openhack2022.cell_sandbox.geom.testLinesIntersection
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.paint.Color
-import javafx.scene.shape.*
-import kotlin.math.PI
+import javafx.scene.paint.CycleMethod
+import javafx.scene.paint.LinearGradient
+import javafx.scene.paint.Stop
+import javafx.scene.shape.ArcType
+import javafx.scene.shape.StrokeLineCap
+import javafx.scene.shape.StrokeLineJoin
+import javafx.scene.text.Font
+import javafx.scene.text.TextAlignment
 import kotlin.math.sqrt
 
 class CellRenderer: Renderer<CellState> {
@@ -19,6 +27,8 @@ class CellRenderer: Renderer<CellState> {
     override fun render(target: CellState, world: World, context: GraphicsContext) {
         context.save()
         with(target) {
+
+            val visualRadius = radius - STROKE_WIDTH/2
 
             val rgb = Color(
                 1 - genome.cyanPigment,
@@ -35,23 +45,29 @@ class CellRenderer: Renderer<CellState> {
                 lineJoin = StrokeLineJoin.ROUND
             }
 
-            val obstacles = calculateObstacles(target, world)
+            val obstacles = calculateObstacles(target, world).map {
+                Pair(
+                    it.first + (it.first to target.center).unit() * STROKE_WIDTH/2,
+                    it.second + (it.second to target.center).unit() * STROKE_WIDTH/2
+                )
+            }
 
             context.beginPath()
 
-            context.arc(
-                center.x, center.y,
-                radius, radius,
-                if (obstacles.isEmpty()) 0.0 else Math.toDegrees((center to obstacles.last().second).angle()),
-                if (obstacles.isEmpty()) 360.0 else
-                    if ((center to obstacles.last().second).angle() > (center to obstacles.first().first).angle())
-                        360 - Math.toDegrees((center to obstacles.last().second).angle() - (center to obstacles.first().first).angle())
-                    else
-                        Math.toDegrees((center to obstacles.first().first).angle() - (center to obstacles.last().second).angle()),
-            )
+            if (obstacles.isEmpty()) {
+                context.arc(center.x, center.y, visualRadius, visualRadius, 0.0, 360.0)
+            } else if (obstacles.last().second != obstacles.first().first) {
+                context.arc(
+                    center.x, center.y,
+                    visualRadius, visualRadius,
+                    Math.toDegrees((center to obstacles.last().second).angle()),
+                    Math.toDegrees((center to obstacles.first().first).positiveAngleDiff(center to obstacles.last().second)),
+                )
+            }
 
             obstacles.forEachIndexed { index, obstacle ->
 
+                context.lineTo(obstacle.first.x, obstacle.first.y)
                 context.lineTo(obstacle.second.x, obstacle.second.y)
 
                 if (index != obstacles.size - 1 && obstacle.second != obstacles[index + 1].first) {
@@ -59,7 +75,7 @@ class CellRenderer: Renderer<CellState> {
 
                     context.arc(
                         center.x, center.y,
-                        radius, radius,
+                        visualRadius, visualRadius,
                         Math.toDegrees((center to obstacle.second).angle()),
                         Math.toDegrees((center to next.first).angle() - (center to obstacle.second).angle())
                     )
@@ -82,7 +98,7 @@ class CellRenderer: Renderer<CellState> {
             drawConnections(context, target, world)
 
             if (world.settings.debugRender)
-                renderDebugInfo(target, context)
+                renderDebugInfo(target, context, obstacles)
         }
         context.restore()
     }
@@ -124,11 +140,12 @@ class CellRenderer: Renderer<CellState> {
         return end
     }
 
-    private fun renderDebugInfo(target: CellState, context: GraphicsContext) {
+    private fun renderDebugInfo(target: CellState, context: GraphicsContext, obstacles: List<Pair<Vector2, Vector2>>) {
         context.save()
         // Angle
+        context.lineCap = StrokeLineCap.BUTT
         context.stroke = Color.GREEN
-        context.lineWidth = 0.5
+        context.lineWidth = 0.25
         val targetAnglePoint = target.center + Vector2(1, 0).rotate(target.angle) * target.radius
         context.strokeLine(target.center.x, target.center.y, targetAnglePoint.x, targetAnglePoint.y)
 
@@ -136,8 +153,34 @@ class CellRenderer: Renderer<CellState> {
         context.stroke = Color.MAGENTA
         context.setLineDashes(1.0, 1.0)
         val splitLinePoint1 = target.center
-        val splitLinePoint2 = target.center + Vector2(1, 0).rotate(target.angle + target.genome.splitAngle) * target.radius
+        val splitLinePoint2 = target.center + Vector2(1, 0)
+            .rotate(target.angle + target.genome.splitAngle) * target.radius
         context.strokeLine(splitLinePoint1.x, splitLinePoint1.y, splitLinePoint2.x, splitLinePoint2.y)
+
+        // Obstacles
+        context.setLineDashes()
+        obstacles.forEach {
+            val optimized = Pair(
+                it.first + (it.first to target.center)*0.1,
+                it.second + (it.second to target.center)*0.1
+            )
+            context.stroke = LinearGradient(
+                optimized.first.x, optimized.first.y,
+                optimized.second.x, optimized.second.y,
+                false, CycleMethod.REPEAT,
+                Stop(0.0, Color.RED),
+                Stop(1.0, Color.ORANGE)
+            )
+            context.strokeLine(optimized.first.x, optimized.first.y, optimized.second.x, optimized.second.y)
+        }
+
+        // ID
+        context.stroke = Color.BLACK
+        context.fill = Color.WHITE
+        context.font = Font.font(target.radius/5)
+        context.textAlign = TextAlignment.CENTER
+        context.strokeText(target.id.toString(), target.center.x, target.center.y)
+        context.fillText(target.id.toString(), target.center.x, target.center.y)
         context.restore()
     }
 
@@ -145,10 +188,10 @@ class CellRenderer: Renderer<CellState> {
         val obstacles = mutableListOf<Pair<Vector2, Vector2>>()
 
         fun addObstacle(a: Vector2, b: Vector2) {
-            val angleA = (target.center to a).angle()
-            val angleB = (target.center to b).angle()
-            val diff = angleB - angleA
-            obstacles += if (diff < -PI || diff in 0.0..PI) Pair(a, b) else Pair(b, a)
+            obstacles += if ((target.center to a).shortestAngularTurn(target.center to b) == COUNTERCLOCKWISE)
+                Pair(a, b)
+            else
+                Pair(b, a)
         }
 
         for (border in world.area.borders.values) {
@@ -161,36 +204,50 @@ class CellRenderer: Renderer<CellState> {
         }
 
         for (cell in world.area.cells.values) {
-            if (cell === target) continue
+            if (cell.id == target.id) continue
             val intersections = testCirclesIntersection(target.center, target.radius, cell.center, cell.radius)
             if (intersections.size == 2)
                 addObstacle(intersections.first(), intersections.last())
         }
 
         obstacles.sortWith { a, b ->
-            if ((target.center to a.first).angle() == (target.center to b.first).angle()) 0 else
-                if ((target.center to a.first).angle() > (target.center to b.first).angle()) 1 else -1
+            val first = (target.center to a.first).angle()
+            val second = (target.center to b.first).angle()
+            if (first == second) 0 else if (first > second) 1 else -1
         }
 
-        obstacles.forEachIndexed { index, obstacle ->
-            if (index != obstacles.size - 1) {
-                val next = obstacles[index + 1]
-                val intersection = testLinesIntersection(obstacle, next)
-                if (intersection != null) {
-                    obstacles[index] = obstacle.copy(second = intersection)
-                    obstacles[index + 1] = next.copy(first = intersection)
+        val obstaclesToRemove = mutableListOf<Pair<Vector2, Vector2>>()
+        do {
+            obstaclesToRemove.clear()
+            obstacles.forEachIndexed { index, obstacle ->
+                val next = obstacles[(index + 1) % obstacles.size]
+                val obstaclePointA = target.center to obstacle.first
+                val obstaclePointB = target.center to obstacle.second
+                val nextPointA = target.center to next.first
+                val nextPointB = target.center to next.second
+
+                if (obstaclePointA.shortestAngularTurn(nextPointA) == COUNTERCLOCKWISE &&
+                    obstaclePointA.shortestAngularTurn(nextPointB) == COUNTERCLOCKWISE &&
+                    obstaclePointB.shortestAngularTurn(nextPointB) == CLOCKWISE
+                ) {
+                    obstaclesToRemove.add(obstacles[(index + 1) % obstacles.size])
                 }
             }
-        }
+            obstacles.removeAll(obstaclesToRemove)
+        } while (obstaclesToRemove.isNotEmpty())
 
-        if (obstacles.size > 1) {
-            val a = obstacles.first().first
-            val b = obstacles.last().first
-            if ((target.center to b).angle() > (target.center to a).angle()) {
-                val intersection = testLinesIntersection(obstacles.last(), obstacles.first())
-                if (intersection != null) {
-                    obstacles[0] = obstacles[0].copy(first = intersection)
-                    obstacles[obstacles.size - 1] = obstacles[obstacles.size - 1].copy(second = intersection)
+        obstacles.forEachIndexed { index, obstacle ->
+            val next = obstacles[(index + 1) % obstacles.size]
+            val obstaclePointB = target.center to obstacle.second
+            val nextPointA = target.center to next.first
+
+            val intersection = testLinesIntersection(obstacle, next)
+            if (intersection != null) {
+                if (obstaclePointB.shortestAngularTurn(nextPointA) == CLOCKWISE) {
+                    if (obstacle.first != intersection && next.second != intersection) {
+                        obstacles[index] = obstacle.copy(second = intersection)
+                        obstacles[(index + 1) % obstacles.size] = next.copy(first = intersection)
+                    }
                 }
             }
         }
